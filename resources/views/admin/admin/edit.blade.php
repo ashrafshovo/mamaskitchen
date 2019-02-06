f42997018820c3470839923ee7d20c09861ca082
Ashraf Hossan Shovo <ashrafshovo@gmail.com>
2019-02-06 20:33:50 +0600

    Admin Notifications

---
+++ resources/views/admin/admin/edit.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('title', 'Admin || Edit')
+
+@push('css')
+
+@endpush
+
+@section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+
+                    @include('layouts.include.msg')
+
+                    <div class="card">
+                        <div class="card-header" data-background-color="purple">
+                            <h4 class="title">Edit Admin</h4>
+                            <!-- <p class="category">Here is a subtitle for this table</p> -->
+                        </div>
+                        <div class="card-content">
+                            <form method="post" action="{{ route('admin.update', $user->id) }}">
+                                @csrf
+                                @method('PUT')
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group label-floating">
+                                            <label class="control-label">Name</label>
+                                            <input type="text" class="form-control" name="Name" value="{{ $user->name }}">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group label-floating">
+                                            <label class="control-label">Email</label>
+                                            <input type="email" class="form-control" name="email" value="{{ $user->email }}">
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="{{ route('admin.index') }}" class="btn btn-danger">Back</a>
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@push('scripts')
+
+@endpush
---
+++ resources/views/admin/admin/index.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.app')
+
+@section('title', 'Admin')
+
+@push('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
+@endpush
+
+@section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <a href="{{ route('admin.create') }}" class="btn btn-primary">Add New</a>
+
+                    <div class="card">
+                        <div class="card-header" data-background-color="purple">
+                            <h4 class="title">All Admin</h4>
+                            <!-- <p class="category">Here is a subtitle for this table</p> -->
+                        </div>
+                        <div class="card-content table-responsive">
+                            <table id="table" class="table" cellspacing="0" width="100%">
+                                <thead class="text-primary">
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Image</th>
+                                    <th>Created At</th>
+                                    <th>Actions</th>
+                                </thead>
+                                <tbody>
+                                    @foreach ($users as $key=>$user )
+                                        <tr>
+                                            <td>{{ $key+1 }}</td>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->email }}
+                                            <td><img class="img-responsive img-thumbnail" style="width: 100px;height: 100px;" src="{{ asset('uploads/user/'. $user->image) }}"></td>
+                                            <td>{{ $user->created_at->diffForHumans() }}</td>
+                                            <td>
+                                                <a href="{{ route('admin.show', $user->id) }}" class="btn btn-primary btn-sm"><i class="material-icons">view</i></a>
+                                                @if(Auth::user()->role == "Admin" && Auth::user() != $user)
+                                                    <a href="{{ route('admin.edit', $user->id) }}" class="btn btn-info btn-sm"><i class="material-icons">edit</i></a>
+
+                                                    
+                                                    <form id="delete-from-{{ $user->id }}" action="{{ route('admin.destroy', $user->id) }}" style="display:none;" method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                    </form>
+                                                    <button class="btn btn-danger btn-sm" type="button" onclick="if(confirm('Are you sure? You want to delete this?')){
+                                                        event.preventDefault();
+                                                        document.getElementById('delete-from-{{ $user->id }}').submit();
+                                                    } else {
+                                                        event.preventDefault();
+                                                    }"><i class="material-icons">delete</i></button>
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@push('scripts')
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js" charset="utf-8"></script>
+    <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js" charset="utf-8"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#table').DataTable();
+        });
+    </script>
+@endpush
